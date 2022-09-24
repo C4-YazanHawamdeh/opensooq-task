de29e16e62a7f7ef05d13aa8f036001f12abe24e
yazan hawamdeh <hawamdey@gmail.com>
2022-09-24 21:35:36 +0300

    add cotroller

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -7,13 +7,13 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
+        <h1 class="display-4">Welcome</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <!-- <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
     </div>
-
+<!-- 
     <div class="body-content">
 
         <div class="row">
@@ -47,7 +47,7 @@ $this->title = 'My Yii Application';
 
                 <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
-        </div>
+        </div> -->
 
     </div>
 </div>
